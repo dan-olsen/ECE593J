004ca446e57cc686620f04645d76f7d1ef8a25bb
Daniel Olsen <dolsen@siu.edu>
2016-02-12 18:22:58 -0600

    Add changes to pointers.c

--- Lab1/pointers.c
+++ Lab1/pointers.c
@@ -6,19 +6,20 @@ int main(){
   char char_array[5] = {'a', 'b', 'c', 'd', 'e'};
   int int_array[5] = {1, 2, 3, 4, 5};
 
-  char *char_pointer;
-  int *int_pointer;
+  unsigned int *pointer;
 
-  char_pointer = int_array;
-  int_pointer = char_array;
+  pointer = (unsigned int*)char_array;
 
   for(i=0; i < 5; i++) {
-    printf("[Integer pointer] points to %p, which contains the char '%c'\n", int_pointer, *int_pointer);
-    int_pointer = int_pointer + 1;
+    printf("[Integer pointer] points to %p, which contains the char '%c'\n", pointer, *pointer);
+    pointer = (unsigned int*)((char*)pointer + 1);
   }
 
+  pointer = (unsigned int*)int_array;
+
   for(i=0; i < 5; i++) {
-    printf("[Char pointer] points to %p, which contains the integer '%d'\n", char_pointer, *char_pointer);
-    char_pointer = char_pointer + 1;
+    printf("[Char pointer] points to %p, which contains the integer '%d'\n", pointer, *pointer);
+    pointer = (unsigned int*)((int*)pointer + 1);
   }
+
 }
