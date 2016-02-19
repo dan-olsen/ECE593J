9785adfe55f58a8dbc7ad2788be4845234a8fec7
Daniel Olsen <dolsen@siu.edu>
2016-02-19 18:52:02 -0500

    Finish section 1 of lab 2

--- Lab2/Section1/input.c
+++ Lab2/Section1/input.c
@@ -4,11 +4,11 @@
 int main(int argc, char* argv[]) {
 	int i, j, k;
 	int x[1000];
-	for(i = 0; i < 10000; ++i){
+	for(i = 0; i < 1000; ++i){
 		x[i] = i;
 	}
 	printf("Enter integer in 0..9999: ");
-	scanf("%d", k);
+	scanf("%d", &k);
 	tester(x, k);
 }
 int tester(int* c, int k) {
