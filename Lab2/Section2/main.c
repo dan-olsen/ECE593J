312633fda651383363d13788cc733d5af92a8126
Daniel Olsen <dolsen@siu.edu>
2016-02-26 15:25:29 -0600

    Add section 2

---
+++ Lab2/Section2/main.c
@@ -0,0 +1,25 @@
+int main()
+{
+	int N = 0;
+	int i;
+	pid_t ret;
+
+	printf("Enter number greater than 10: ");
+	scanf("%d", N);
+
+	for(i = 0; i < N; ++i)
+	{
+		ret = fork();
+
+		if(ret == 0)
+		{
+
+		}
+		else
+		{
+
+		]
+	}
+
+	return 0;
+}
