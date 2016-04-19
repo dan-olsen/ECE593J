ca81801ece72fe54796e30e95a68f8dfed8aeb8a
Daniel Olsen <dolsen@siu.edu>
2016-04-18 22:58:01 -0500

    Start lab5

--- Lab5/lab5.c
+++ Lab5/lab5.c
@@ -7,21 +7,47 @@
 struct node
 {
 	//Struct for the node of the queue
+	pid_t pid;
+	struct node *next;
 };
 
 struct queue
 {
 	//Struct for the queue
+	struct node *front;
+	struct node *back;
+
+	int size;
 };
 
-void enqueue()
+void enqueue(struct queue *q, pid_t p)
 {
+	struct node* n = (struct node*) malloc (sizeof(struct node));
 
+    n->pid = p;
+    n->next = NULL;
+
+    if (q->front == NULL)
+	{
+        q->front = n;
+    } else{
+        q->back->next = n;
+    }
+
+    q->back = n;
+    q->size++;
 }
 
-int dequeue()
+pid_t dequeue(struct queue *q)
 {
-	
+    struct node* front = q->front;
+    pid_t p = front->pid;
+
+    q->front = front->next;
+    q->size--;
+
+    free(front);
+    return p;
 }
 
 void termchild()
@@ -32,52 +58,67 @@ void termchild()
 
 int main(int argc, char const *argv[])
 {
+	int programCount = 0, i;
+	pid_t child;
+	int qt;
+
+	struct queue q;
+	q.front = NULL;
+	q.back = NULL;
+	q.size = 0;
+
 	//Variable initialization
 	signal(SIGCHLD, termchild);
-	if(argc>2)
+
+	if(argc > 2)
 	{
-		qt = 1000*atoi(&argv[1][0]);//Here you need the qt
+		qt = 1000 * atoi(&argv[1][0]); //Here you need the qt
 	}
 	else
 	{
-		printf("%s qt prog1 [prog2] ... [prog[N}\n", argv[0]);
-		exit(−1);
+		printf("Usage: %s qt prog1 [prog2] ... [prog[N}\n", argv[0]);
+		exit(-1);
 	}
-	
-	
-	for(...)
+
+	for(programCount = 0, i = 2; i < argc - 2; i++)
 	{
+		programCount++;
 		//Fork, execl processes and enqueue
 		printf("Message from father: Creating program %s\n", argv[i]);
 
-		if(...)
+		if((child = fork()) == 0)
 		{
 			execl(argv[i], argv[i], NULL);
 		}
-		
-		enqueue the process
+
+		enqueue(&q, child);
 	}
 
-	sleep(1);//Small safe dealy
+	sleep(1);//Small safe delay
+
 	printf("\nI am the Scheduler and I will now begin scheduling my programs:\n");
-	
-	while(queue is not empty)//Scheduling loop
+
+	while(q.front != NULL)//Scheduling loop
 	{
-		send the signal SIGCONT to the first element in queue
+		pid_t p = dequeue(&q);
+
+		kill(p, SIGCONT);
 
 		usleep(qt);
 
-		if(child has not finished)
-		{
-			send the signal SIGUSR1 to the first element in queue
-			usleep(1000);
-			dequeue and re−enqueue
-		}
-		else
-		{
-			printf("A child is dead\n");
-			remove from list
-			mark child as dead
-		}
+		//if(child has not finished)
+		//{
+			//send the signal SIGUSR1 to the first element in queue
+			//usleep(1000);
+			//dequeue and re−enqueue
+		//}
+		//else
+		//{
+			//printf("A child is dead\n");
+			//remove from list
+			//mark child as dead
+		//}
 	}
+
+	return 0;
 }
