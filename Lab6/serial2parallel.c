320e2048801ffbecb9ad2e9fb4abe99f8c769cb3
Daniel Olsen <dolsen@siu.edu>
2016-04-18 23:35:11 -0500

    finish lab 6

--- Lab6/serial2parallel.c
+++ Lab6/serial2parallel.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define MAX_NUM_THREADS 200
+
 /* A task that takes some time to complete. The id identifies distinct
    tasks for printed messages. */
 void *task(int id) {
@@ -18,6 +20,16 @@ void *task(int id) {
 /* Same as 'task', but meant to be called from different threads. */
 void *threaded_task(void *t) {
   // The sub-routine called by each thread
+  long id;
+  id = (long)t;
+  printf("Task %ld started\n", id);
+  int i;
+  double result = 0.0;
+  for (i = 0; i < 1000000; i++) {
+    result = result + sin(i) * tan(i);
+  }
+  printf("Task %ld completed with result %e\n", id, result);
+  pthread_exit((void*) t);
 }
 
 /* Run 'task' num_tasks times serially. */
@@ -32,7 +44,38 @@ void *serial(int num_tasks) {
    call to 'task'. */
 void *parallel(int num_tasks)
 {
- //here you create threads
+  //here you create threads
+  long i = 0;
+  int r;
+  void *status;
+
+  pthread_attr_t attr;
+  pthread_t threads[MAX_NUM_THREADS];
+
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+  for(i = 0; i < num_tasks; i++)
+  {
+    r = pthread_create(&threads[i], &attr, threaded_task, (void *)i);
+    
+    if (r) {
+      printf("ERROR; return code from pthread_create() is %d\n", r);
+      exit(-1);
+    }
+  }
+
+  pthread_attr_destroy(&attr);
+
+  for(i = 0; i < num_tasks; i++)
+  {
+    r = pthread_join(threads[i], &status);
+
+    if (r) {
+      printf("ERROR; return code from pthread_join() is %d\n", r);
+      exit(-1);
+    }
+  }
 }
 
 void *print_usage(int argc, char *argv[]) {
